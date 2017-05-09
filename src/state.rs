6897b151c81beea56b917e4ea294183e62075f67
Jason Orendorff <jason.orendorff@gmail.com>
2017-05-08 20:33:42 -0500

    Factor out `simulate_flow()` and add a few tests.
    There are still no test for attacking, but I'll add a few of those, too.

--- src/state.rs
+++ src/state.rs
@@ -42,7 +42,7 @@ pub struct State {
 pub struct Player(pub usize);
 
 /// The state of a node that is occupied by some player.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Occupied {
     /// The player who controls this node.
     pub player: Player,
@@ -52,7 +52,6 @@ pub struct Occupied {
 
     /// How much goop this node holds. Ranges from 0 to MAX_GOOP.
     pub goop: usize,
-
 }
 
 pub const MAX_GOOP: usize = 15;
@@ -111,57 +110,12 @@ impl State {
         self.rng.shuffle(&mut outflow_list);
 
         while let Some((from_index, to_index)) = outflow_list.pop() {
-            match index_mut_pair(&mut self.nodes, from_index, to_index) {
-                // We shouldn't have generated a pair for an empty source, and
-                // when we clear a node we're supposed to remove pairs from
-                // `outflow` that originate there.
-                (&mut None, _) => panic!("outflow from empty node"),
+            let (from_node, to_node) = index_mut_pair(&mut self.nodes, from_index, to_index);
+            let attacked = simulate_flow(from_node, to_node);
 
-                // Source has no goop. No effect.
-                (&mut Some(Occupied { goop: 0, .. }), _) => (),
-
-                // Goop flowing into an unoccupied node. New player claims ownership.
-                (&mut Some(Occupied { player, ref mut goop, .. }),
-                 &mut ref mut to @ None) => {
-                    *goop -= 1;
-                    *to = Some(Occupied { player, outflows: vec![], goop: 1 });
-                },
-
-                // Goop flowing into a node occupied by the same player.
-                (&mut Some(Occupied { player: from_player, goop: ref mut from_goop, .. }),
-                 &mut Some(Occupied { player: to_player,   goop: ref mut to_goop, .. }))
-                    if from_player == to_player =>
-                {
-                    if *from_goop > 0 && *to_goop < MAX_GOOP {
-                        *from_goop -= 1;
-                        *to_goop += 1;
-                    }
-                }
-
-                // Goop flowing into a node occupied by another player, but
-                // doesn't clear it. All outflow from destination stopped.
-                (&mut Some(Occupied { goop: ref mut from_goop, .. }),
-                 &mut Some(Occupied { outflows: ref mut to_outflows,
-                                       goop:     ref mut to_goop, .. }))
-                    if *to_goop > 1 =>
-                {
-                    *from_goop -= 1;
-                    *to_goop -= 1;
-                    to_outflows.clear();
-                    // Since all outflow from `to` is cancelled, remove any
-                    // pending pairs from `outflow_list`.
-                    outflow_list.retain(|&(from, _)| from != to_index);
-                },
-
-                // Goop flowing into an occupied node, succeeds in clearing it.
-                (&mut Some(Occupied { player, goop: ref mut from_goop, .. }),
-                 &mut ref mut to) => {
-                    *from_goop -= 1;
-                    *to = Some(Occupied { player, outflows: vec![], goop: 0 });
-                    // Since all outflow from `to` is cancelled, remove any
-                    // pending pairs from `outflow_list`.
-                    outflow_list.retain(|&(from, _)| from != to_index);
-                }
+            if attacked {
+                // `to_node` is being attacked. Disregard any outflows from it this turn.
+                outflow_list.retain(|&(from, _)| from != to_index);
             }
         }
     }
@@ -210,6 +164,200 @@ impl State {
             }
         }
     }
+}
+
+/// Simulate goop flow from a given cell `from_node` to another cell, `to_node`.
+///
+/// This only simulates flow in that particular direction;
+/// if two neighboring cells are both trying to ooze into each other,
+/// then the State::flow() function will helpfully call this twice,
+/// once for each direction.
+///
+/// See the tests to learn the detailed rules of this function's behavior.
+///
+/// Return true if `from_node` attacked `to_node`—that is, if the two nodes are
+/// occupied by two different players, and any goop flowed. (The caller needs to
+/// know about this, because in this case it must stop `to_node`'s outflows.)
+///
+fn simulate_flow(from_node: &mut Option<Occupied>, to_node: &mut Option<Occupied>) -> bool {
+    match (from_node, to_node) {
+        // We shouldn't have generated a pair for an empty source, and
+        // when we clear a node we're supposed to remove pairs from
+        // `outflow` that originate there.
+        (&mut None, _) => panic!("outflow from empty node"),
+
+        // Source has no goop. No effect.
+        (&mut Some(Occupied { goop: 0, .. }), _) => false,
+
+        // Goop flowing into an unoccupied node. New player claims ownership.
+        (&mut Some(Occupied { player, ref mut goop, .. }),
+         &mut ref mut to @ None) => {
+            *goop -= 1;
+            *to = Some(Occupied { player, outflows: vec![], goop: 1 });
+            false
+        },
+
+        // Goop flowing into a node occupied by the same player.
+        (&mut Some(Occupied { player: from_player, goop: ref mut from_goop, .. }),
+         &mut Some(Occupied { player: to_player,   goop: ref mut to_goop, .. }))
+            if from_player == to_player =>
+        {
+            if *from_goop > 0 && *to_goop < MAX_GOOP {
+                *from_goop -= 1;
+                *to_goop += 1;
+            }
+            false
+        }
+
+        // Goop flowing into a node occupied by another player, but
+        // doesn't clear it. All outflow from destination stopped.
+        (&mut Some(Occupied { goop: ref mut from_goop, .. }),
+         &mut Some(Occupied { outflows: ref mut to_outflows,
+                               goop:     ref mut to_goop, .. }))
+            if *to_goop > 1 =>
+        {
+            *from_goop -= 1;
+            *to_goop -= 1;
+            to_outflows.clear();
+            true
+        },
+
+        // Goop flowing into an occupied node, succeeds in clearing it.
+        (&mut Some(Occupied { player, goop: ref mut from_goop, .. }),
+         &mut ref mut to) => {
+            *from_goop -= 1;
+            *to = Some(Occupied { player, outflows: vec![], goop: 0 });
+            true
+        }
+    }
+}
+
+#[test]
+fn test_flow_into_unoccupied_cell() {
+    // The kingdom of Florin is invading the kingdom of Guilder and flooding it with goop.
+    let mut florin = Some(Occupied { player: Player(1), outflows: vec![2], goop: 15 });
+    let mut guilder = None;
+
+    // This isn't considered an attack, since Guilder was completely unoccupied.
+    assert_eq!(simulate_flow(&mut florin, &mut guilder), false);
+
+    // One unit of goop flowed. The newly occupied territory now belongs to player 1.
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2], goop: 14 }));
+    assert_eq!(guilder, Some(Occupied { player: Player(1), outflows: vec![], goop: 1 }));
+}
+
+#[test]
+fn test_flow_empties_cell() {
+    // Same scenario, except this time there is just one unit of goop in Florin.
+    let mut florin = Some(Occupied { player: Player(1), outflows: vec![2], goop: 1 });
+    let mut guilder = None;
+
+    // As above, this isn't considered an attack.
+    assert_eq!(simulate_flow(&mut florin, &mut guilder), false);
+
+    // One unit of goop flowed, leaving Florin emptied of goop (but still
+    // considered occupied by player 1).
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2], goop: 0 }));
+    assert_eq!(guilder, Some(Occupied { player: Player(1), outflows: vec![], goop: 1 }));
+
+    // In the next tick of the game, no more goop flows, because Florin is now empty.
+    assert_eq!(simulate_flow(&mut florin, &mut guilder), false);
+
+    // The state after this second simulated step is therefore exactly the same as before.
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2], goop: 0 }));
+    assert_eq!(guilder, Some(Occupied { player: Player(1), outflows: vec![], goop: 1 }));
+}
+
+#[test]
+fn test_no_flow_from_empty_cell() {
+    // Florin, alas, has ambitions of conquest but no goop to carry them out with.
+    let mut florin = Some(Occupied { player: Player(1), outflows: vec![2, 3, 4], goop: 0 });
+
+    // Florin can try to flow into a cell that's never been occupied, but since
+    // Florin has no goop, the cell does *not* become occupied.
+    let mut zolot = None;
+    assert_eq!(simulate_flow(&mut florin, &mut zolot), false);
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2, 3, 4], goop: 0 }));
+    assert_eq!(zolot, None);
+
+    // Nor can Florin attack a cell occupied by an opponent with goop.
+    let mut guilder = Some(Occupied { player: Player(2), outflows: vec![5, 6], goop: 1 });
+    // The attempt does not count as an attack, since no goop flowed.
+    assert_eq!(simulate_flow(&mut florin, &mut guilder), false);
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2, 3, 4], goop: 0 }));
+    assert_eq!(guilder, Some(Occupied { player: Player(2), outflows: vec![5, 6], goop: 1 }));
+    
+    // Same deal even if Guilder also has no goop.
+    guilder = Some(Occupied { player: Player(2), outflows: vec![5, 6], goop: 0 });
+    assert_eq!(simulate_flow(&mut florin, &mut guilder), false);
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2, 3, 4], goop: 0 }));
+    assert_eq!(guilder, Some(Occupied { player: Player(2), outflows: vec![5, 6], goop: 0 }));
+
+    // Nor can Florin reinforce Guilder if they happen to be occupied by the same player.
+    guilder = Some(Occupied { player: Player(1), outflows: vec![5, 6], goop: 0 });
+    assert_eq!(simulate_flow(&mut florin, &mut guilder), false);
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2, 3, 4], goop: 0 }));
+    assert_eq!(guilder, Some(Occupied { player: Player(1), outflows: vec![5, 6], goop: 0 }));
+}
+
+#[test]
+fn test_friendly_flow() {
+    // Florin is sending goop into the friendly neighboring province of Krugerrand.
+    let mut florin     = Some(Occupied { player: Player(1), outflows: vec![2], goop: 8 });
+    let mut krugerrand = Some(Occupied { player: Player(1), outflows: vec![3], goop: 0 });
+    // This isn't an attack, since the same player occupies both.
+    assert_eq!(simulate_flow(&mut florin, &mut krugerrand), false);
+    // A unit of goop actually flowed.
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2], goop: 7 }));
+    // Krugerrand received the goop, and its outflows are unaffected.
+    assert_eq!(krugerrand, Some(Occupied { player: Player(1), outflows: vec![3], goop: 1 }));
+
+    // It works even if the destination already has more goop than Florin.
+    krugerrand.as_mut().unwrap().goop = 9;
+    assert_eq!(simulate_flow(&mut florin, &mut krugerrand), false);
+    assert_eq!(florin.unwrap().goop, 6);
+    assert_eq!(krugerrand.unwrap().goop, 10);
+}
+
+#[test]
+fn test_friendly_flow_empties_cell() {
+    // Florin can send its only unit of goop to a neighboring empty or nonempty friendly cell.
+    let mut florin = Some(Occupied { player: Player(1), outflows: vec![2, 3], goop: 1 });
+    let mut guilder = Some(Occupied { player: Player(1), outflows: vec![4], goop: 0 });
+    // This isn't an attack, since the same player occupies both.
+    assert_eq!(simulate_flow(&mut florin, &mut guilder), false);
+    // A unit of goop actually flowed.
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2, 3], goop: 0 }));
+    // Guilder received the goop, and its outflows are unaffected.
+    assert_eq!(guilder, Some(Occupied { player: Player(1), outflows: vec![4], goop: 1 }));
+
+    // Restore Florin's 1 unit of goop and try again.
+    florin.as_mut().unwrap().goop = 1;
+    assert_eq!(simulate_flow(&mut florin, &mut guilder), false);
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2, 3], goop: 0 }));
+    assert_eq!(guilder, Some(Occupied { player: Player(1), outflows: vec![4], goop: 2 }));
+}
+
+#[test]
+fn test_friendly_flow_max_goop() {
+    // Florin can't pump any more goop into its friendly neighbor Pfennig,
+    // which has the maximum amount already.
+    let mut florin  = Some(Occupied { player: Player(1), outflows: vec![2, 3], goop: 3 });
+    let mut pfennig = Some(Occupied { player: Player(1), outflows: vec![4], goop: MAX_GOOP });
+
+    assert_eq!(simulate_flow(&mut florin, &mut pfennig), false);
+    assert_eq!(florin, Some(Occupied { player: Player(1), outflows: vec![2, 3], goop: 3 }));
+    assert_eq!(pfennig, Some(Occupied { player: Player(1), outflows: vec![4], goop: MAX_GOOP }));
+}
+
+#[test]
+#[should_panic]
+fn test_flow_from_unoccupied_cell() {
+    // This should never happen, because the simulator won't pass an unoccupied
+    // cell to simulate_flow().
+    let mut zolot = None;
+    let mut zorkmid = None;
+    simulate_flow(&mut zolot, &mut zorkmid);
 }
 
 /// Actions that can be taken on a `State`.
